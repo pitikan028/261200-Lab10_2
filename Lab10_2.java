74d3f7b3aa1ac16736cc050407310cab19da6dd0
pitikan028 <pitikan_kan@cmu.ac.th>
2025-09-18 09:03:47 +0700

    Update Lab10_2.java

--- Lab10_2.java
+++ Lab10_2.java
@@ -1,5 +1,6 @@
 public class Lab10_2 {
     public static void main(String[] args) {
+        // Create Xpay object and set values
         Xpay xpay = new XpayImpl();
         xpay.setCreditCardNo("4789565874102365");
         xpay.setCustomerName("Somchai Jaidee");
@@ -8,8 +9,10 @@ public class Lab10_2 {
         xpay.setCardCVVNo((short) 235);
         xpay.setAmount(2565.23);
 
+        // Wrap Xpay object with Adapter to PayD
         PayD payD = new XpayToPayDAdapter(xpay);
 
+        // Print values via PayD interface
         System.out.println("Credit Card Number: " + payD.getCreditCardNo());
         System.out.println("Card Owner Name: " + payD.getCardOwnerName());
         System.out.println("Card Expiry Date: " + payD.getCardExpMonthYear());
