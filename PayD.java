c22fde85babc3d89c351919ea1ac512a4e197a35
pitikan028 <pitikan_kan@cmu.ac.th>
2025-09-18 09:04:13 +0700

    Update PayD.java

--- PayD.java
+++ PayD.java
@@ -1,9 +1,10 @@
+// PayD interface (Target system we want to adapt to)
 public interface PayD {
-    long getCreditCardNo();
-    String getCardOwnerName();
-    String getCardExpMonthYear();
-    Integer getCVVNo();
-    Double getTotalAmount();
+    long getCreditCardNo();          // Credit card number (long)
+    String getCardOwnerName();       // Card holder name
+    String getCardExpMonthYear();    // Expiry in MM/YY format
+    Integer getCVVNo();              // CVV as Integer
+    Double getTotalAmount();         // Amount
 
     void setCreditCardNo(long creditCardNo);
     void setCardOwnerName(String cardOwnerName);
